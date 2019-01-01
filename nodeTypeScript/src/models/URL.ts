b8276c2a552bc92a07c9e62fb4569cf4d5226b10
seanr89 <srafferty89@gmail.com>
2019-01-01 19:28:04 +0000

    new class created for URL params

---
+++ nodeTypeScript/src/models/URL.ts
@@ -0,0 +1,13 @@
+
+/**
+ * handle URL parameter details
+ */
+export class URL {
+    /**
+     * constructor
+     * @param id : the unique record for the URL
+     * @param address : the http url address of the site!
+     */
+    constructor(public id: number, public address: string) {
+    }
+}
