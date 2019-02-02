026efc35c523632114001572bd1245f938223510
seanr89 <srafferty89@gmail.com>
2019-02-02 21:28:35 +0000

    added commands to handle building, tagging and pushing to docker

--- nodeTypeScript/scripts/BuildAndPushToDocker.sh
+++ nodeTypeScript/scripts/BuildAndPushToDocker.sh
@@ -3,3 +3,10 @@ set -e
 
 #Navigate back a directory to the console app
 cd ../
+
+#Build a docker container locally with the current project
+docker build -t nodetypescript .
+#Tag the container
+docker tag nodetypescript seanraff89/nodetypescript
+#Push the container to my personal hub with latest param
+docker push seanraff89/nodetypescript:latest
